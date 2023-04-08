da759c4f4db43d74e49348bbde02a48aa436aac6
Safromanov <Safroman-ov@yandex.ru>
2023-04-08 17:15:27 +0700

    refactor: исправлены связи между интерфейсами, добавлена база данных

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -2,9 +2,16 @@ package ru.yandex.practicum.filmorate.service.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-public interface FilmService extends FilmStorage {
+import java.util.Collection;
+import java.util.List;
+
+public interface FilmService {
+    Collection<Film> findAll();
+    Film create(Film film);
+    Film update(Film film);
+    Film getFilm(long id);
+    List<Film> getPopularFilm(int size);
     void likeFilm(Film film, User user);
     void dislikeFilm(Film film, User user);
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -3,7 +3,12 @@ package ru.yandex.practicum.filmorate.service.user;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.util.List;
+
 public interface UserService extends UserStorage {
+    User getUser(long id);
+    List<User> getFriends(long id);
+    List<User> getCommonFriends(long userId, long friendId);
     void friend(User user, User anotherUser);
     void unfriend(User user, User anotherUser);
 }
