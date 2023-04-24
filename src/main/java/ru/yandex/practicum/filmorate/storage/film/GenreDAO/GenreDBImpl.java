3bb0a16255588266a81ebdaf567596da6085e763
Safromanov <Safroman-ov@yandex.ru>
2023-04-24 21:03:58 +0700

    refactor: в методе addGenresToFilm update заменён batchUpdate

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/GenreDAO/GenreDBImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/GenreDAO/GenreDBImpl.java
@@ -2,23 +2,21 @@ package ru.yandex.practicum.filmorate.storage.film.GenreDAO;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
 public class GenreDBImpl implements GenreDB {
-
     private final NamedParameterJdbcTemplate jdbcTemplate;
-
     private final RowMapper<Genre> genreMapper;
+
 
     public Genre getGenre(int id) {
         String sql = "SELECT * FROM genre WHERE genre_id = :genre_id;";
@@ -44,16 +42,30 @@ public class GenreDBImpl implements GenreDB {
     }
 
     public List<Genre> addGenresToFilm(long filmId, List<Genre> genres) {
+        if (genres == null) return new ArrayList<>();
+        var countGenres = genres.size();
         String sqlGenreInfo = "MERGE INTO genre_films(film_id, genre_id) VALUES (:film_id, :genre_id);";
-        Map<String, Object> params = new HashMap<>();
-        params.put("film_id", filmId);
-
-        if (genres != null)
-            for (var genre : genres) {
-                params.put("genre_id", genre.getId());
-                jdbcTemplate.update(sqlGenreInfo, params);
-            }
+        SqlParameterSource[] sources = new SqlParameterSource[countGenres];
+        for (int i = 0; i < countGenres; i++) {
+            Map<String, Number> params = new HashMap<>();
+            params.put("film_id", filmId);
+            params.put("genre_id", genres.get(i).getId());
+            sources[i] = new MapSqlParameterSource(params);
+        }
+        jdbcTemplate.batchUpdate(sqlGenreInfo, sources);
         return findGenresFilm(filmId);
+//  Альтернативный вариант без batchUpdate
+//        String sqlGenreInfo = "MERGE INTO genre_films(film_id, genre_id) VALUES (:film_id, :genre_id);";
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("film_id", filmId);
+//
+//        if (genres != null)
+//            for (var genre : genres) {
+//                params.put("genre_id", genre.getId());
+//                jdbcTemplate.update(sqlGenreInfo, params);
+//            }
+//        return findGenresFilm(filmId);
+
     }
 
     public List<Genre> updateGenresFilm(long filmId, List<Genre> genres) {
