9f9f1c888fc00ca190a7c13215a22caabcd3d706
Safromanov <Safroman-ov@yandex.ru>
2023-05-01 14:24:43 +0700

    fix

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -2,10 +2,17 @@ package ru.yandex.practicum.filmorate.service.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
+
+    Collection<User> findAll();
+
+    User add(User user);
+
+    User update(User user);
 
     Optional<User> getUser(long id);
 
